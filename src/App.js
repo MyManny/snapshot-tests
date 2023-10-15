4de23a86f5d00d7eb47e1cf1134f7c220885fa07
MyManny <gmail.com>
2023-10-14 23:37:23 -0700

    updated app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import GitHubCard from './components/GitHubCard';
-
-
+import './App.css'
+import GitHubCard from './components/GitHubCard'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 function App() {
   return (
-    <div className="App">
-      <GitHubCard />
+    <div className="App pt-5" >
+      <Row>
+        <Col style={{display: 'flex', justifyContent: 'center'}}>
+          <GitHubCard />
+        </Col>
+      </Row>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
